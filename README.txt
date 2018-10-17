b7008e9168a16844c61c34ac68579e5dd5d3f3d8
Jonathan Mathews <jonathan.mathews@mines.sdsmt.edu>
2018-10-17 15:21:36 -0500

    Complete sequential version of hash_breaker

--- README.txt
+++ README.txt
@@ -1,1 +1,21 @@
 # CSC_410_hash_breaker
+
+The makefile is set up to make two programs:
+* hash
+* mathews_seq
+
+hash returns the hash for a string given as a command line argument
+
+usage: ./hash cba
+
+mathews_seq does an exhaustive search to find the string that produced
+a specific hash
+
+usage: ./mathews_seq <hash>
+
+The two can be used in conjunction by calling:
+
+./mathews_seq $(./hash cba)
+
+This will create the hash for "cba" and pass that as the argument to 
+mathews_seq which will then try to determine the corresponding string
--- mathews_seq.c
+++ mathews_seq.c
@@ -4,13 +4,12 @@
 #include <math.h>
 #include <stdlib.h>
 
+const unsigned numUseable = 26;
+const char useable[] = "abcdefghijklmnopqrstuvwxyz";
 unsigned long long hash(char *str);
 
 void makeStr( char* str, unsigned numChars, unsigned index)
 {
-    unsigned numUseable = 26;
-    char useable[] = "abcdefghijklmnopqrstuvwxyz";
-
     if( index >= pow(numUseable, numChars) )
     {
         printf("Error: Index too big\n");
@@ -23,7 +22,9 @@ void makeStr( char* str, unsigned numChars, unsigned index)
         unsigned subIndex = index % numUseable;
         index = index / numUseable;
 
-        printf("Assigning %c to location %d\n", useable[subIndex], numChars-i);
+        #ifdef DEBUG
+            printf("Assigning %c to location %d\n", useable[subIndex], numChars-i);
+        #endif
         str[numChars-i] = useable[subIndex];
     }
     
@@ -31,13 +32,31 @@ void makeStr( char* str, unsigned numChars, unsigned index)
 
 int main( int argc, char* argv[])
 {
-    unsigned numChars = atof(argv[1]);
-    unsigned index = atof(argv[2]);
-    char str[16];
+    if( argc !=2 )
+    {
+        printf("Error: incorrect usage\nUsage: ./mathews_seq hash\n");
+        return 1;
+    }
+    unsigned long long inputHash = atoll(argv[1]);
+    unsigned long long tempHash = 0;
+    const unsigned maxSize = 16;
+    char str[maxSize];
 
-    makeStr(str, numChars, index);
+    for( unsigned numChars = 1; numChars <= maxSize; numChars++)
+    {
+        unsigned maxIndex = pow(numUseable, numChars);
+        for( unsigned i = 0; i < maxIndex; i++ )
+        {
+            makeStr(str, numChars, i);
+            tempHash = hash(str);
+            if( tempHash == inputHash)
+                break;
+        }
+        if( tempHash == inputHash)
+            break;
+    }
 
-    printf("String: %s  Hash: %d\n", str, hash(str));
+    printf("String: %s  Hash: %lld\n", str, hash(str));
 
 	return 0;
 }
