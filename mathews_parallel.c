de97530e7534d587c16b918371acd04949cf34de
Jonathan Mathews <jonathan.mathews@mines.sdsmt.edu>
2018-10-22 23:18:12 -0500

    Upgrade to O(log(N)) time from O(N)
    I realized that the hash function is monotonically increasing with
    increasing string length and alphabetic value. With this information, we
    can do a variation of a binary search instead of a linear search. We can
    check T different locations in the domain where T is the number of
    threads. We can then reduce our domain to one of the T+1 subregions for
    the next iteration. In this way, our complexity becomes O(log_{T+1}(N)),
    where N is the number of possible solutions in our space.

--- mathews_parallel.c
+++ mathews_parallel.c
@@ -9,7 +9,7 @@ const unsigned numUseable = 26;
 const char useable[] = "abcdefghijklmnopqrstuvwxyz";
 unsigned long long hash(char *str);
 
-void makeStr( char* str, unsigned index)
+void makeStr( char* str, unsigned long long index)
 {
     unsigned numChars = 0;
     for( numChars = 1; numChars < 16; numChars++)
@@ -27,7 +27,7 @@ void makeStr( char* str, unsigned index)
     {
         
         printf("Error: Index too big\n");
-        printf("index: %d numChars: %d", index, numChars);
+        printf("index: %lld numChars: %d", index, numChars);
         str = "";
         return;
     }
@@ -49,26 +49,67 @@ int main( int argc, char* argv[])
 {
     if( argc !=2 )
     {
-        printf("Error: incorrect usage\nUsage: ./mathews_seq hash\n");
+        printf("Error: incorrect usage\nUsage: ./mathews_seq <hash>\n");
         return 1;
     }
     unsigned long long inputHash = atoll(argv[1]);
-    const unsigned maxSize = 10;
-    char solutionStr[maxSize];
+    const unsigned maxSize = 11;
+    unsigned size = maxSize;
+    //if( size > 11 )
+    //{
+    //    printf("Error: index will overflow unsigned long long\n");
+    //    return 1;
+    //}
+    char solutionStr[maxSize+1];
     unsigned long long solutionHash = 0;
 
-    unsigned long long maxIndex = pow(numUseable, maxSize);
+    //unsigned long long maxIndex = pow(numUseable, maxSize);
     unsigned long long globIndex = 0;
+    unsigned long long upperBound = 0;
+    for( unsigned i = 1; i < size+1; i++)
+    {
+        upperBound += pow(numUseable, i);
+    }
+    #ifdef DEBUG
+    printf("upperBound: %lld \n", upperBound);
+    #endif
+    makeStr(solutionStr, upperBound-1);
+    #ifdef DEBUG
+    printf("upperStr: %s \n", solutionStr);
+    #endif
+    unsigned long long upperHash = hash(solutionStr);
+    #ifdef DEBUG
+    printf("upperHash: %lld \n", upperHash);
+    #endif
+    unsigned long long lowerBound = 0;
+    #ifdef DEBUG
+    printf("lowerBound: %lld \n", lowerBound);
+    #endif
+    makeStr(solutionStr, lowerBound);
+    #ifdef DEBUG
+    printf("lowerStr: %s \n", solutionStr);
+    #endif
+    unsigned long long lowerHash = hash(solutionStr);
+    #ifdef DEBUG
+    printf("lowerHash: %lld \n", lowerHash);
+    #endif
+    unsigned long long interval;
+
     #pragma omp parallel
     {
         unsigned long long tempHash = 0;
-        char tempStr[maxSize];
+        char tempStr[maxSize+1];
         unsigned long long myIndex = 0;
+        unsigned long long numThreads = omp_get_num_threads();
+        interval = (upperBound - lowerBound)/(numThreads-1) + 1;
         #pragma omp critical
         {
-            myIndex = globIndex++;
+            myIndex = lowerBound + (globIndex++*interval);
+            #ifdef DEBUG
+            printf("myIndex: %lld \n", myIndex);
+            #endif
         }
-        while( solutionHash == 0 && myIndex < maxIndex)
+        while( solutionHash == 0 && lowerBound < upperBound)
         {
             makeStr(tempStr, myIndex);
             tempHash = hash(tempStr);
@@ -77,13 +118,36 @@ int main( int argc, char* argv[])
                 solutionHash = tempHash;
                 strcpy(solutionStr, tempStr);
             }
-            #pragma omp critical
+            #pragma omp critical (lower)
+            if( tempHash > lowerHash && tempHash < inputHash )
             {
-                myIndex = ++globIndex;
+                lowerBound = myIndex;
+                lowerHash = tempHash;
+            }
+            #pragma omp critical (upper)
+            if( tempHash < upperHash && tempHash > inputHash )
+            {
+                upperBound = myIndex;
+                upperHash = tempHash;
+            }
+            #pragma omp barrier
+            #pragma omp single 
+            {
+                interval = (upperBound - lowerBound)/(numThreads+1) + 1;
+                globIndex = 1;
+            }
+            #pragma omp critical (index)
+            {
+                myIndex = lowerBound + (globIndex++*interval);
             }
         }
     }
     
+    if( solutionHash == 0 )
+    {
+        printf("Couldn't find string in range\n");
+        return 1;
+    }
     printf("String: %s  Hash: %lld\n", solutionStr, hash(solutionStr));
 
 	return 0;
